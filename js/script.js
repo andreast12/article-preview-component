40d67390a14ed73d9a5864a1f2bf87fb7f859f22
andreast12 <andreast12.id@gmail.com>
2020-11-29 15:49:29 +0700

    desktop version

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
 const shareBtn = document.querySelector('.fa-share');
 const sharePopup = document.querySelector('.share-popup');
 let popup = false;
+const x = window.matchMedia('(min-width: 992px)');
 
-shareBtn.addEventListener('mouseenter', (e) => {
-	e.target.classList.add('shareBtn-click');
-});
+shareBtn.addEventListener('mouseenter', e => e.target.classList.add('shareBtn-click'));
 
-shareBtn.addEventListener('mouseout', (e) => {
+shareBtn.addEventListener('mouseout', e => {
 	if(!popup) {
 		e.target.classList.remove('shareBtn-click');
 	}
 });
 
+console.log(x);
+
 shareBtn.addEventListener('click', (e) => {
-	if(!popup) {
-		sharePopup.style.transform = 'translateY(-100%)';
-		popup = true;
+	if(x.matches) {
+		if(!popup) {
+			sharePopup.style.transform = 'scale(1)';
+			popup = true;
+		} else {
+			sharePopup.style.transform = 'scale(0)';
+			popup = false;
+		}	
 	} else {
-		sharePopup.style.transform = 'translateY(100%)';
-		popup = false;
+		if(!popup) {
+			sharePopup.style.transform = 'translateY(-100%)';
+			popup = true;
+		} else {
+			sharePopup.style.transform = 'translateY(100%)';
+			popup = false;
+		}	
 	}
 });
